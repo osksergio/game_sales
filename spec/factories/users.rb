8e37130ce6b04595cd6c4c079fbefca1c3398b74
Sergio Oseko <osk.sergio@gmail.com>
2023-04-27 21:29:50 -0300

    feat: criação da factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'test@test.com' }
+    password { '12345678' }
+    password_confirmation { '12345678' }
+    age { 18 }
+    is_admin { true }
   end
 end
