c2e5b49577988d768bee0e4fc0a81270039face7
Sergio Oseko <osk.sergio@gmail.com>
2023-04-29 08:50:43 -0300

    test: implementados alguns testes de factory / model

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :game do
-    title { "MyString" }
-    genre { "MyString" }
-    platform { "MyString" }
-    release { "2023-04-29" }
+    title { 'Resident Evil 4' }
+    genre { 'Horror' }
+    platform { 'Windows' }
+    release { '2007-04-29' }
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory' do
+    context 'when using standard factory' do
+      it { expect(build(:game)).to be_valid }
+    end
+  end
+
+  describe 'validations' do
+    context 'when game has no title' do
+      it { expect(build(:game, title: nil)).not_to be_valid }
+    end
+
+  end
 end
