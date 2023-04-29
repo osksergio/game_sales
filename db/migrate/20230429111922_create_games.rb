a5cd057eea20b896fde8395446a44813aa92979e
Sergio Oseko <osk.sergio@gmail.com>
2023-04-29 08:22:08 -0300

    feat: criação da model game

---
+++ db/migrate/20230429111922_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.string :genre
+      t.string :platform
+      t.date :release
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/games.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :game do
+    title { "MyString" }
+    genre { "MyString" }
+    platform { "MyString" }
+    release { "2023-04-29" }
+  end
+end
