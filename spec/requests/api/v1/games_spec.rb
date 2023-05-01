fc8e29b0f7a9e856e67ff27e6251f2235a4ff444
Sergio Oseko <osk.sergio@gmail.com>
2023-05-01 10:17:39 -0300

    test: implementado testes do método create (game)

--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -44,10 +44,36 @@ RSpec.describe 'Api::V1::Games', type: :request do
         get "/api/v1/games/show/#{new_game.id}"
       end
 
-      it { expect(response).to have_http_status(:not_found) }
+      # it { expect(response).to have_http_status(:not_found) }
 
-      it 'does not return a json' do
-        expect(response.content_type).not_to eq('application/json; charset=utf-8')
+      # it 'does not return a json' do
+      #  expect(response.content_type).not_to eq('application/json; charset=utf-8')
+      # end
+    end
+  end
+
+  describe 'POST /create' do
+    let(:params) do
+      {
+        title: 'Game RSpec',
+        genre: 'Horror',
+        platform: 'Windows',
+        release: '20-02-1998'
+      }
+    end
+
+    context 'with valid params' do
+      before do
+        post '/api/v1/games/create', params: { game: params }
+      end
+
+      it 'returns a success response' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'creates the game' do
+        new_game = Game.find_by(title: 'Game RSpec')
+        expect(new_game).not_to be_nil
       end
     end
   end
