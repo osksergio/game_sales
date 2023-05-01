bc8f68cd00e3d20e69fb47af287ceca45da4636e
Sergio Oseko <osk.sergio@gmail.com>
2023-05-01 09:02:50 -0300

    test: implementado teste do método show (game - está ocorrendo um erro ao rodar os testes)

--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -24,4 +24,31 @@ RSpec.describe 'Api::V1::Games', type: :request do
       expect(JSON.parse(response.body).size).to eq(2)
     end
   end
+
+  describe 'GET /show' do
+    let(:new_game) { create(:game) }
+
+    context 'when game exist' do
+      before { get "/api/v1/games/show/#{new_game.id}" }
+
+      it { expect(response).to have_http_status(:ok) }
+
+      it 'returns with json' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+
+    context 'when game does not exist' do
+      before do
+        new_game.destroy
+        get "/api/v1/games/show/#{new_game.id}"
+      end
+
+      it { expect(response).to have_http_status(:not_found) }
+
+      it 'does not return a json' do
+        expect(response.content_type).not_to eq('application/json; charset=utf-8')
+      end
+    end
+  end
 end
